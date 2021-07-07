a4d45efb6ba74fe5c2f493fc6729bde8d02b94ad
J3SUNG <wptjd6141@naver.com>
2021-07-08 02:47:51 +0900

    house

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="div">
+    <div className="house">
       <div className="room"></div>
       <div className="toper"></div>
       <div className="desk1"></div>
