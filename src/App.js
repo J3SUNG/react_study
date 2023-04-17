bb806261e7ac5e7770e360b2196e55f87f24ca4c
wptjd6141@naver.com <wptjd6141@naver.com>
2023-04-18 01:33:36 +0900

    react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
         <div className="greet">Hello, I'm Jetty!</div>
+        <div className="greet">React & JSX</div>
       </header>
-    </div>
+    </>
   );
 }
 
