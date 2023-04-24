43c64c00adce06c9460333dc6ab75fa136787f44
wptjd6141@naver.com <wptjd6141@naver.com>
2023-04-24 23:02:59 +0900

    add component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-function App() {
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+
+const App = () => {
   return (
-    <>
-      <p>
-        <header>
-          <h1>Hi</h1>
-        </header>
-        <div className="react">리액트</div>
-        {/* <div>주석 처리된 내용</div> */}
-        <div>// 주석처리 되지 않은 내용 </div>
-        <div>/* 주석처리 되지 않은 내용 */</div>
-      </p>
-    </>
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
   );
-}
+};
+
 export default App;
