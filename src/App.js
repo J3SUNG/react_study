f8239497c71e8f68dbb2797b552635ced79208b1
wptjd6141@naver.com <wptjd6141@naver.com>
2023-04-24 21:53:31 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 function App() {
   return (
     <>
-      <header>
-        <h1>Hi</h1>
-      </header>
-      <div className="react">리액트</div>
-      {/* <div>주석 처리된 내용</div> */}
-      <div>// 주석처리 되지 않은 내용 </div>
-      <div>/* 주석처리 되지 않은 내용 */</div>
+      <p>
+        <header>
+          <h1>Hi</h1>
+        </header>
+        <div className="react">리액트</div>
+        {/* <div>주석 처리된 내용</div> */}
+        <div>// 주석처리 되지 않은 내용 </div>
+        <div>/* 주석처리 되지 않은 내용 */</div>
+      </p>
     </>
   );
 }
