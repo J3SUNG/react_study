f724a1b6a7a78c30404488a95f83055e23d1cbd6
wptjd6141@naver.com <wptjd6141@naver.com>
2024-12-08 21:45:07 +0900

    Feat: mongoDB 연결

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://jetty:6141@boilerplate.uhaze.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
