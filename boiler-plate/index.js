87fc953d60145ab81f30194dd3862792d54e14ee
wptjd6141@naver.com <wptjd6141@naver.com>
2024-12-10 23:07:10 +0900

    Feat: 회원가입 기능 구현, 이슈 발생
    - bodyParser는 express를 통해 사용하도록 변경됨
    - 여러 설정값들은 default로 변경됨
    - node 버전 문제로 db 연결 실패 -> 22버전으로 업데이트

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const mongoose = require("mongoose");
+mongoose.set("debug", true);
 mongoose
   .connect(
-    "mongodb+srv://jetty:6141@boilerplate.uhaze.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+    "mongodb+srv://JETTY:6141@boilerplate.uhaze.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate"
   )
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", async (req, res) => {
+  const user = new User(req.body);
+  console.log("req.body: ", req.body);
+
+  try {
+    await user.save();
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    console.log("에러: ", err);
+    return res.status(400).json({ success: false, err });
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
