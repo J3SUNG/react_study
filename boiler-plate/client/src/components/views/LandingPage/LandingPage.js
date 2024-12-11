5af504e94f953fbce9f348d1b106da88f51924bc
wptjd6141@naver.com <wptjd6141@naver.com>
2024-12-12 03:23:20 +0900

    Feat: 서버 연결 테스트
    - proxy 서버 localhost:5000
    - http-proxy-middleware 사용하려 했으나, package.json에서 쉽게 proxy 서버 설정 가능해서 변경
    - 백엔드 연결 이슈 : post요청을 get요청으로 보냄

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function LandingPage() {
+  useEffect(() => {
+    axios
+      .post("/api/users/login", {
+        email: "jt@co.kr",
+        password: "123456",
+      })
+      .then((response) => console.log(response));
+  }, []);
   return <div>LandingPage</div>;
 }
 
