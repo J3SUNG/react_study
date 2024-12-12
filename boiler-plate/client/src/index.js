3b4e6ab4dab1b1e5f4cfe7846ac85a32e069d93a
wptjd6141@naver.com <wptjd6141@naver.com>
2024-12-13 05:59:01 +0900

    Feat: redux toolkit 설치
    - redux에서 createStore 함수가 더 이상 사용되지 않는 경고 발생
    - redux 팀에서 redux toolkit의 사용을 권장

--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
 import "antd/dist/antd.css";
+import { configureStore } from "@reduxjs/toolkit";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = configureStore({
+  reducer: Reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(promiseMiddleware, ReduxThunk),
+  devTools: process.env.NODE_ENV !== "production",
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
