408603f9e1fe47017762bcb35e5a2ef543999a48
wptjd6141@naver.com <wptjd6141@naver.com>
2024-12-13 07:04:42 +0900

    Feat: index.js에서 sotre분리

--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import "antd/dist/reset.css";
-import { configureStore } from "@reduxjs/toolkit";
-import promiseMiddleware from "redux-promise";
-import Reducer from "./_reducers";
+import store from "./sotre";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const store = configureStore({
-  reducer: Reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(promiseMiddleware),
-  devTools: process.env.NODE_ENV !== "production",
-});
 
 root.render(
   <React.StrictMode>
