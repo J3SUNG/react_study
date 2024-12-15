63e8f42f2f9c12155f053a445a59fb996e1c0014
wptjd6141@naver.com <wptjd6141@naver.com>
2024-12-15 21:15:53 +0900

    Feat: db 설정
    - neon postgres 설정
    - drizzle orm 설정

---
+++ chat-gpt/db/schema.ts
@@ -0,0 +1,11 @@
+import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
+
+export const user = pgTable("user", {
+  id: serial("id"),
+  name: text("name"),
+  email: text("email"),
+  password: text("password"),
+  role: text("role").$type<"admin" | "customer">(),
+  created_at: timestamp("created_at"),
+  updated_at: timestamp("updated_at"),
+});
