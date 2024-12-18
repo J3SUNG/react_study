c978da5e3532b8f5d636bbe9503e58d5352894a1
wptjd6141@naver.com <wptjd6141@naver.com>
2024-12-18 20:49:46 +0900

    Feat: 사용자 상태 관리와 세션 처리 기능 추가
    - 사용자 상태 업데이트 로직 추가
    - 세션 검증으로 사용자 정보 가져오기 구현
    - 로그아웃 기능 구현

--- chat-gpt/app/(chat)/layout.tsx
+++ chat-gpt/app/(chat)/layout.tsx
@@ -1,16 +1,19 @@
 import { Header } from "@/components/chat/Header";
 import { Sidebar } from "@/components/chat/Sidebar";
+import { UserProvider } from "@/components/chat/UserProvider";
 
 export default function ChatLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="md:flex h-full">
-      <div className="hidden md:block w-[300px]">
-        <Sidebar />
+    <UserProvider>
+      <div className="md:flex h-full">
+        <div className="hidden md:block w-[300px]">
+          <Sidebar />
+        </div>
+        <div className="flex flex-col flex-1 h-full overflow-y-auto">
+          <Header />
+          {children}
+        </div>
       </div>
-      <div className="flex flex-col flex-1 h-full overflow-y-auto">
-        <Header />
-        {children}
-      </div>
-    </div>
+    </UserProvider>
   );
 }
--- chat-gpt/components/chat/Chat.tsx
+++ chat-gpt/components/chat/Chat.tsx
@@ -11,6 +11,7 @@ import { useModelStore } from "@/store/model";
 import { useParams, useRouter } from "next/navigation";
 import { addMessages, createConversation } from "@/actions/conversation";
 import { CHAT_ROUTES } from "@/constants/routes";
+import { useUserStore } from "@/store/user";
 
 type Props = {
   initialMessages?: TMessage[];
@@ -19,6 +20,7 @@ type Props = {
 export function Chat({ initialMessages }: Props) {
   const router = useRouter();
   const params = useParams<{ conversationId: string }>();
+  const user = useUserStore((state) => state.user);
   const { messages, setMessages, input, handleInputChange, handleSubmit } = useChat({
     onFinish: async (message) => {
       if (!params.conversationId) {
@@ -49,14 +51,14 @@ export function Chat({ initialMessages }: Props) {
   return (
     <div className="flex flex-col w-[80%] h-full mx-auto">
       <div className="flex-1">
-        {messages.length === 0 ? (
+        {!params.conversationId && messages.length === 0 ? (
           <Empty />
         ) : (
           <>
             {messages.map((message) => (
               <Message
                 key={message.id}
-                name={"user"}
+                name={user.name}
                 content={message.content}
                 role={message.role}
               />
